dcd35a1a43ffca6473cf9f25cc1fa4696394ed74
Ghezlene Doballah <ghyzounette@gmail.com>
2020-03-18 09:05:25 +0100

    student-info

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -4,10 +4,16 @@
   <div class="students">
     @foreach (config('students.students') as $student)
       <div class="student">
-        <img src="{{$student['img']}}" alt="{{$student['nome']}}">
-        <h3>{{$student['nome']}} ({{$student['eta']}} anni)</h3>
-        <h4>assunt{{($student['genere'] == 'm') ? 'o' : 'a'}} da {{$student['azienda']}} come {{$student['ruolo']}}</h4>
-        <p>{{$student['descrzione']}}</p>
+        <div class="info">
+          <div class="image">
+            <img src="{{$student['img']}}" alt="{{$student['nome']}}">
+          </div>
+          <div class="text">
+            <h3>{{$student['nome']}} ({{$student['eta']}} anni)</h3>
+            <h4>assunt{{($student['genere'] == 'm') ? 'o' : 'a'}} da {{$student['azienda']}} come {{$student['ruolo']}}</h4>
+        </div>
+        </div>
+          <p>{{$student['descrzione']}}</p>
       </div>
 
     @endforeach
