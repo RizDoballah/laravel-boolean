5364cb8624b37c75355eb070970b5e4122fcde6e
Ghezlene Doballah <ghyzounette@gmail.com>
2020-03-18 07:47:17 +0100

    student

--- config/students.php
+++ config/students.php
@@ -8,6 +8,7 @@
         'eta' => 25,
         'azienda' => 'DISC SPA',
         'ruolo' => 'web developer',
+        'genere' => 'm',
         'descrzione' => 'Da giocatore professionista di basket a sviluppatore web. 6 mesi di impegno da MVP e un memorabile tap-in targato Boolean hanno garantito ad Alessandro un solido futuro come web developer.'
       ],
       [
@@ -16,6 +17,7 @@
         'eta' => 24,
         'azienda' => 'Prima Assicurazioni',
         'ruolo' => 'junior software engineer',
+        'genere' => 'f',
         'descrzione' => 'A 24 anni, dopo aver conseguito il diploma linguistico ha deciso di intraprendere fin da subito un percorso nel mondo Tech. Ad oggi ricopre il ruolo di Junior Software Engineer.'
       ],
       [
@@ -24,6 +26,7 @@
         'eta' => 36,
         'azienda' => 'Zen Agency',
         'ruolo' => 'web developer',
+        'genere' => 'f',
         'descrzione' => 'Ha scoperto la passione per l\'informatica creando un blog di psicologia, ambito in cui si era specializzata durante gli studi. Il suo cuore però non vuol sentir ragione e Loana cambia carriera e diventa una ricercatissima sviluppatrice web.'
       ],
       [
@@ -32,6 +35,7 @@
         'eta' => 29,
         'azienda' => 'ArmadioVerde',
         'ruolo' => 'web developer',
+        'genere' => 'm',
         'descrzione' => 'Dopo la laurea magistrale in scienze umanistiche e sociali, si concentra sullo sviluppo di strategie marketing. Decide di cambiare vita, questa volta puntando sullo sviluppo Front-End.'
       ],
     ]
--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.layout')
 @section('main')
-  @foreach (config('students.students') as $student)
-    {{$student['nome']}}
-  @endforeach
+
+  <div class="students">
+    @foreach (config('students.students') as $student)
+      <div class="student">
+        <img src="{{$student['img']}}" alt="{{$student['nome']}}">
+        <h3>{{$student['nome']}} ({{$student['eta']}} anni)</h3>
+        <h4>assunt{{($student['genere'] == 'm') ? 'o' : 'a'}} da {{$student['azienda']}} come {{$student['ruolo']}}</h4>
+        <p>{{$student['descrzione']}}</p>
+      </div>
+
+    @endforeach
+
+  </div>
 
 @endsection
