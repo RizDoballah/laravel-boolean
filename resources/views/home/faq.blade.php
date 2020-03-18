aac3f9a48ed42aa5a434c74ae02acd2e74cc7c9c
Ghezlene Doballah <ghyzounette@gmail.com>
2020-03-18 19:29:37 +0100

    faq

--- resources/views/home/faq.blade.php
+++ resources/views/home/faq.blade.php
@@ -1,6 +1,36 @@
 @extends('layouts.layout')
 @section('header')
-  <h2>Domande frequenti</h2>
-  <p>Le nostre risposte a tutte le tue domande. Non trovi la risposta che stai cercando? Contattaci e saremo felici di darti una mano.</p>
+  <div class="faq-header">
+    <h2>Domande frequenti</h2>
+    <p>Le nostre risposte a tutte le tue domande. Non trovi la risposta che stai cercando? Contattaci e saremo felici di darti una mano.</p>
+  </div>
+@endsection
+
+@section('main')
+  <div class="faq-main">
+    <div class="prima">
+      <h3>Prima del corso</h3>
+      <ul>
+        <li>Chi può fare il corso Boolean?</li>
+        <li>Che competenze devo avere per iniziare il corso?</li>
+        <li>In cosa consiste il processo di selezione?</li>
+        <li>Posso seguire il corso mentre lavoro o studio?</li>
+        <li>Di cosa ho bisogno per seguire il corso?</li>
+        <li>Questo corso è una valida alternativa alla laurea?</li>
+      </ul>
+    </div>
+    <div class="dopo">
+      <h3>Dopo il corso</h3>
+      <ul>
+        <li>Chi può fare il corso Boolean?</li>
+        <li>Che competenze devo avere per iniziare il corso?</li>
+        <li>In cosa consiste il processo di selezione?</li>
+        <li>Posso seguire il corso mentre lavoro o studio?</li>
+        <li>Di cosa ho bisogno per seguire il corso?</li>
+        <li>Questo corso è una valida alternativa alla laurea?</li>
+      </ul>
+    </div>
+  </div>
+
 
 @endsection
